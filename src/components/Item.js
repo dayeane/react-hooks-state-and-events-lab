1413320a95d7053baee18c62df35921df4edbe95
Dayeane <dayeane85@gmail.com>
2021-11-02 21:30:56 -0700

    React State Lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import {React, useState} from "react";
 
 function Item({ name, category }) {
+  const [isDark, setIsDark] = useState(false)
+  const appClass = isDark ? "in-cart" : ""
+
+  function addToCart(e) {
+    setIsDark(!isDark)
+  }
+
+
   return (
-    <li className="">
+    <li className={appClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={addToCart} className="add">Add to Cart</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,22 @@
-import React from "react";
+import {React, useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelected] = useState("All")
+
+  function changeSelection(e) {
+    setSelected(e.target.value)
+  }
+
+  function byCategory(item) {
+    return (selectedCategory === item.category || selectedCategory === "All")
+  }
+
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select  onChange={changeSelection} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +24,11 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {items.map((item) => {
+          if (byCategory(item)) {
+            return <Item key={item.id} name={item.name} category={item.category} />
+          }
+        })}
       </ul>
     </div>
   );
